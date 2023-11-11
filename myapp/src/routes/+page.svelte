6e98f9167598d8728b3b6315923510b7bfe80285
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-11-11 12:30:50 +0100

    Set up SvelteKit with Svelte

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Bærum-portefølje</h1>
+
+<a href="/barumjazzfestival">Bærum Jazzfestival</a>
+<br>
+<a href="/dans-sorost-norge">Dans Sørøst-Norge</a>
---
+++ myapp/src/routes/dans-sorost-norge/+page.svelte
@@ -0,0 +1,6 @@
+<main>
+<h1>Dans Sørøst-Norge</h1>
+<a href="">Produksjon</a>
+<a href="">Formidling</a>
+<a href="">Om oss</a>
+</main>
