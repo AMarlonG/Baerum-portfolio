2b90b4d9b80b32e0a11ed4a16e8c10cd6fb47e42
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-11-14 15:59:04 +0100

    Added image content

--- my-app/app/dans-sorost-norge/page.tsx
+++ my-app/app/dans-sorost-norge/page.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import dansebilde from '../../assets/dans.jpg';
+import styles from './dans.module.css';
+
 export default function Page() {
   return (
     <>
       <h1>Dans Sørøst-Norge</h1>
+      <Link href='/'>Produksjon</Link>
+      <Link href='/'>Formidling</Link>
+      <Image
+        className={styles.image}
+        src={dansebilde}
+        alt='Danser som hopper'
+        priority
+      />
     </>
   );
 }
